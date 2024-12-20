55f35714ebc3898f9ffb1c1291d8a781ef122122
yoorw <37107949+yoorw@users.noreply.github.com>
2024-12-20 17:09:35 +0000

    skillsmember added

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember(){
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        isDeveloper: true
+    }
+    return member;
+}
